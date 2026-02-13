1237e9e0b6dfb37b0a4b79be849d3c41b2c3fa2c
joshik77 <joshikr658@gmail.com>
2026-02-13 15:57:53 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Pallindrome Checker Management System ");
+        System.out.println("VERSION:1.0 ");
+        System.out.println("System Initialized Sucessfully");
+
+    }
+}
---
+++ src/PallindromCheckerApp.java
@@ -0,0 +1,5 @@
+public class PallindromCheckerApp {
+    public static void main(String[] args){
+
+    }
+}
