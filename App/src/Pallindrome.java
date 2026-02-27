d3ff846de8ac0ffef2a2d99013b0f0d44688e4d2
joshik77 <joshikr658@gmail.com>
2026-02-27 14:37:54 +0530

    [joshik77]completed uc1

---
+++ App/src/Pallindrome.java
@@ -0,0 +1,7 @@
+public class Pallindrome {
+    static void main() {
+        System.out.println("Welcome to the palindrome Checker Management System");
+        System.out.println("Version:1.0");
+        System.out.println("System initialized successfully");
+    }
+}
