f8e84252922704e8a53ac1d8a1e2ad2921fb86d5
joshik77 <joshikr658@gmail.com>
2026-03-09 15:54:10 +0530

    uc6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,35 +1,42 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class PalindromeCheckerApp {
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=================================");
-        System.out.println("   Palindrome Checker - UC5");
-        System.out.println("   Stack Based Approach");
+        System.out.println(" Palindrome Checker - UC6");
+        System.out.println(" Queue + Stack Approach");
         System.out.println("=================================");
 
         // Take input
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        // Create Stack
+        // Create Stack and Queue
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        // Push characters into stack
+        // Push to stack and enqueue to queue
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
         }
 
         boolean isPalindrome = true;
 
-        // Pop and compare
-        for (int i = 0; i < input.length(); i++) {
-            char poppedChar = stack.pop();
-            if (input.charAt(i) != poppedChar) {
+        // Compare dequeue vs pop
+        while (!stack.isEmpty()) {
+            char fromStack = stack.pop();      // Last In First Out
+            char fromQueue = queue.remove();   // First In First Out
+
+            if (fromStack != fromQueue) {
                 isPalindrome = false;
                 break;
             }
